2705c2bedb5de1f111c85649997b0ef22d6bdabf
Pablo Velez <pvelez@bp-3.com>
2019-06-23 21:55:13 -0500

    Add the draw and save image function using the egd library

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,6 +10,23 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write!("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn ({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
@@ -18,8 +35,8 @@ defmodule Identicon do
       vertical = div(index, 5) * 50
 
       top_left = {horizontal, vertical}
-      bottom_left = {horizontal + 50, vertical + 50}
-      {top_left, bottom_left}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
     end
     %Identicon.Image{image | pixel_map: pixel_map}
   end
